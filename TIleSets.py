d0ed4794d28531aa57699f1117801941596b7cf0
lmifflen <lmifflen@gmail.com>
2022-04-06 19:13:31 -0600

    file loading throught mts

---
+++ TIleSets.py
@@ -0,0 +1,9 @@
+## uploading tilesets to the database
+tilesets upload-source miffll populated-places-source populated_places.geojson.ld
+
+
+tilesets create miffll.park-pathways --recipe park-pathways.json --name "park pathways" --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
+
+tilesets upload-source miffll BowRiver-Inglewood BowRiverInglewoodCon.geojson --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
+
+tilesets upload-source miffll WesternHeadworksCanalPathwayN WesternHeadworksCanalPathwayN.geojson --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
