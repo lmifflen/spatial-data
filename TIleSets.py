b85d62b7351a02b9078cac9b712ef089631b1b32
lmifflen <lmifflen@gmail.com>
2022-04-07 11:20:43 -0600

    updated some of the methods used for uploading

--- TIleSets.py
+++ TIleSets.py
@@ -1,9 +1,21 @@
 ## uploading tilesets to the database
 tilesets upload-source miffll populated-places-source populated_places.geojson.ld
 
+## deleteing tilesets
+tilesets delete-source miffll PumphousePark
 
-tilesets create miffll.park-pathways --recipe park-pathways.json --name "park pathways" --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
+##creating tilesets
+tilesets create miffll.missing-links --recipe missing-links.json --name "missing links"
+
+## updating tileset recipes
+ tilesets update-recipe miffll.missing-links missing-links.json
+ 
+ ## publish tilesets
+ tilesets publish miffll.missing-links
+
+
+tilesets create miffll.missing-links --recipe missing-links.json --name "missing links" --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
 
 tilesets upload-source miffll BowRiver-Inglewood BowRiverInglewoodCon.geojson --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
 
-tilesets upload-source miffll WesternHeadworksCanalPathwayN WesternHeadworksCanalPathwayN.geojson --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
+tilesets upload-source miffll BowRiverCycleBridges BowRiverCycleBridges.geojson --token sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
