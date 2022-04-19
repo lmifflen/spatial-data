77f025c351e3c19250986bfeba09e2745229b9d2
lmifflen <lmifflen@gmail.com>
2022-04-19 09:55:17 -0600

    park and ride locations are added

--- TIleSets.py
+++ TIleSets.py
@@ -5,13 +5,13 @@ tilesets upload-source miffll VistaHeights-source ./ParkAndBike/VistaHeights.geo
 tilesets delete-source miffll PumphousePark
 
 ##creating tilesets
-tilesets create miffll.missing-links --recipe missing-links.json --name "missing links"
+tilesets create miffll.ParkAndBike --recipe ./ParkAndBike/ParkandBike.json --name "Park and Bike"
 
 ## updating tileset recipes
  tilesets update-recipe miffll.missing-links missing-links.json
  
  ## publish tilesets
- tilesets publish miffll.park-pathways
+ tilesets publish miffll.ParkAndBike
  
  export MAPBOX_ACCESS_TOKEN=sk.eyJ1IjoibWlmZmxsIiwiYSI6ImNsMW5tNXQ2MzB2dDkzaW9iazh6dGVtbmEifQ.j8ONnlDXTT8sNv8C2U-kXA
 
